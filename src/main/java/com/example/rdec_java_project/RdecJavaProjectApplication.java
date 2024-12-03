efff1837cf038fe7f45829268f1cf67108310c28
arun kumar <java.kumar.arun@gmail.com>
2024-12-03 15:17:00 +0530

    project ready

---
+++ src/main/java/com/example/rdec_java_project/RdecJavaProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.rdec_java_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdecJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdecJavaProjectApplication.class, args);
+
+		System.out.println("App started");
+	}
+
+}
---
+++ src/test/java/com/example/rdec_java_project/RdecJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rdec_java_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdecJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
